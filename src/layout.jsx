f8bbce83be0f8d8cb32564a6cf789098c325c92b
Henrique Mattos <henrique.mattos@sap.com>
2018-03-08 15:46:30 +0100

    Added G Suite Referral button to landing page

--- src/layout.jsx
+++ src/layout.jsx
@@ -51,6 +51,9 @@ export default class Layout extends App {
                 </div>
                 <div className="hero-foot">
                     <div className="container has-text-centered">
+                        <a href="https://gsuite.google.com/intl/pt-BR/landing/partners/referral/trial.html?utm_source=sign-up&utm_medium=referralbutton1&utm_campaign=apps-referral-program&utm_content=R3RD7V8" target="_blank"><img src="https://connect.googleforwork.com/servlet/JiveServlet/showImage/102-18621-1-87860/Copy+of+638303_Copy+of+Sonic_DarwIT_GSuite_PT-BR.jpg" /></a>
+                    </div>
+                    <div className="container has-text-centered">
                         <div className="fb-like" data-href="https://www.facebook.com/visualworks.web" data-layout="button" data-action="recommend" data-size="large" data-show-faces="false" data-share="false"></div>
                     </div>
                 </div>
