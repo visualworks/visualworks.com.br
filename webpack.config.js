72a382080308194d405cd7a4632beaa129a6b460
Henrique Mattos <henrique@visualworks.com.br>
2019-03-09 14:41:25 +0000

    Fixed build mode to production

--- webpack.config.js
+++ webpack.config.js
@@ -69,7 +69,7 @@ module.exports = {
     plugins: [
         new webpack.DefinePlugin({
             "process.env": {
-                "NODE_ENV": JSON.stringify(process.env.NODE_ENV)
+                "NODE_ENV": process.env.NODE_ENV ? JSON.stringify(process.env.NODE_ENV) : "production"
             }
         }),
         new UglifyJsPlugin(),
