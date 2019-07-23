b52f8c45647b004a4b9d07ee1ac93e8a96002b1c
Henrique Mattos <henrique@visualworks.com.br>
2019-07-23 21:10:59 +0200

    Fixing entry point and resolve path in Webpack
    Also fixing component loading in index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import "scss/style.scss";
 import React from "react";
 import ReactDOM from "react-dom";
-import Layout from "./components/layout";
+import Layout from "components/layout";
 
 const layout = <Layout />;
 const targetDiv = document.getElementById("app");
